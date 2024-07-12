7b5cb879f2db49680718fbab9c2d10eb17c376f4
Ricardo Belmar <spamsbelmar@gmail.com>
2024-07-11 20:04:52 -0400

    footer arreglado

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,16 @@ $(window).scroll(function () {
         navbar.removeClass('scrolled');
     }
 });
+$(document).ready(function () {
+    $('a.nav-link').on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - $('.navbar').outerHeight()
+            }, 900, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
